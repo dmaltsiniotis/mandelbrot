7ea880f5b6c148702878b159fbe6dc746f2a10fb
Demetri Maltsiniotis <git@quantumhive.com>
2020-10-01 14:06:09 -0400

    Update to .NET Core 3.1, NuGet packages, and README.

--- Madelbrot/mandelbrot.fs
+++ Madelbrot/mandelbrot.fs
@@ -4,7 +4,7 @@ open FSharp.Collections.ParallelSeq
 
 let max_iterations = 100 // This effectively controls the level of detail
 let max_rgb = 255
-let mandelbrotTest c z = Complex.Pow(z, 2.0) + c // This is the actual Mandelbrot set "algorithm"
+let mandelbrotTest (c:Complex) z = Complex.Pow(z, 2.0) + c // This is the actual Mandelbrot set "algorithm"
 
 let rec inSet c iterations z =
     let i = iterations - 1
@@ -15,15 +15,15 @@ let rec inSet c iterations z =
 let colorMod iteration =
     let iteration = if iteration < 0 then 0 else iteration
     let interprolatedColorValue = (iteration * max_rgb) / max_iterations
-    Color.FromArgb(interprolatedColorValue, interprolatedColorValue, interprolatedColorValue) // Maybe we can enhance this to return preetty colors in the future, but grey-scale looks good for now.
+    Color.FromArgb(interprolatedColorValue, interprolatedColorValue, interprolatedColorValue) // Maybe we can enhance this to return pretty colors in the future, but grey-scale looks good for now.
 
 [<EntryPoint>]
-let complex argv =
+let main _ =
      let startTime = System.DateTime.Now
      let p0 = new Complex(0.0, 0.0)
-     let plotScalingFactor = 1000.0 // This essentially controls how large/detailed the final bitmap will be. I can only get this up to about 5000 on a 64bit windows system with 64gb of ram.
+     let plotScalingFactor = 1000.0 // This essentially controls how large/detailed the final bitmap will be. I can only get this up to about 5000 on a 64bit windows system with 64gb of ram. This is probably limited by System.Drawing.Bitmap.
      let stepSize = 1.0 / plotScalingFactor
-     let plotsize = 2.0 // This should always be 2.0 because when you plot the Mandelbrot set on a coordinate plane, it's range on the x-axis is -1 to 2.0.
+     let plotsize = 2.0 // This should always be 2.0 because when you plot the Mandelbrot set on a coordinate plane, its range on the x-axis is -1 to 2.0.
 
      let plot =
       seq { for i in plotsize * -1.0 .. stepSize .. plotsize do
--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # A Mandelbrot fractal generator written in F\#
 
+![Mandlebrot Set](https://raw.githubusercontent.com/dmaltsiniotis/mandelbrot/master/Renders/mandelbrot_lowres.bmp)
+
+## Info
+
+This project currently supports .NET Core 3.1. However, there is an annoying dependency on System.Drawing in order to output the final bitmap. Running the console application will generate file called mandelbrot.bmp in the current run directory.
+
 ## Tips
 
 To adjust the size of the final size of the bitmap, try varying the "plotScalingFactor" variable between 100, 500, 1000, and if you're feeling ambitious, 5000. I personally have not been able to create a bitmap on 64bit windows with 64gb of ram greater than 5000x5000 using System.Drawing.Bitmap. Your mileage may vary though as the Bitmap class is a thin wrapper over GDI+ and shouldn't be used here in the first place.
 
-## Example outputs:
+## Examples
 
 These renders were run in debug mode on an Intel Core i9 7980 XE (18 real cores, 36 hyper-threaded cores) with 64 GB RAM. This whole project was an exercise in discovering a way to utilize my idle CPU time :(
 
@@ -14,3 +20,7 @@ These renders were run in debug mode on an Intel Core i9 7980 XE (18 real cores,
 
 [plotScalingFactor 5000: High Resolution ~6-7 minutes render time](https://raw.githubusercontent.com/dmaltsiniotis/mandelbrot/master/Renders/mandelbrot_highres.bmp)
 
+## TODO
+
+* It would be fantastic to change the "rendering engine" of System.Drawing.Bitmap to support something cross-platorm and efficient.
+* Colorize the iterations to make the plot more pretty.
